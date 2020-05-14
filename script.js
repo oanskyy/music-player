bca944352afa724d6f1ff4ae4010c693a897fc7a
codessbabe <oana.anghelachi@gmail.com>
2020-05-14 09:56:56 +0200

    Create load, play, pause song

---
+++ script.js
@@ -0,0 +1,56 @@
+//  Select DOM elements to use
+const musicContainer = document.getElementById('music-container'); 
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next'); 
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress'); 
+const progressContainer = document.getElementById('progress-container'); 
+const title = document.getElementById('title'); 
+const cover = document.getElementById('cover'); 
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele', 'inRuins']; 
+
+// Keep track of song 
+const songIndex = 2; 
+
+// Initially load song details into DOM 
+loadSong(songs[songIndex]); 
+
+// Update song details 
+function loadSong(song) { 
+  title.innerText = song; 
+  audio.src = `music/${song}.mp3`;
+  cover.src = `images/${song}.jpg`;
+}
+
+// play song
+function playSong() { 
+  musicContainer.classList.add('play'); 
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
+  // with the Audio API we just call a method called .play() 
+  audio.play();
+}
+
+// pause song
+function pauseSong() { 
+  musicContainer.classList.remove('play'); 
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
+
+  audio.pause();
+}
+
+
+// Event listeners 
+playBtn.addEventListener('click', () => { 
+  const isPlaying = musicContainer.classList.contains('play'); 
+  if(isPlaying) { 
+    pauseSong();
+  } else { 
+    playSong();
+  }
+})
